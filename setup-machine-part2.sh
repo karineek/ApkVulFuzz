2f31b1b853359d761d5d972844b77b40377f0f7b
Karine EM <karineven79@gmail.com>
2026-03-16 18:02:54 +0000

    Create setup-machine-part2.sh

---
+++ setup-machine-part2.sh
@@ -0,0 +1,33 @@
+yes | sdkmanager --sdk_root="$ANDROID_SDK_ROOT" --licenses
+
+sdkmanager --sdk_root="$ANDROID_SDK_ROOT" \
+  "platform-tools" \
+  "emulator" \
+  "platforms;android-34" \
+  "system-images;android-34;google_apis;x86_64"
+
+sdkmanager "build-tools;34.0.0"
+
+adb version
+sdkmanager --list | head
+avdmanager list target | head
+apkanalyzer --help | head
+
+avdmanager create avd -n test34 -k "system-images;android-34;google_apis;x86_64"
+
+emulator -list-avds
+
+emulator -avd test34 -no-window -no-audio -no-boot-anim -no-metrics -no-snapshot > /tmp/emulator.log 2>&1 &
+adb wait-for-device
+until [ "$(adb shell getprop sys.boot_completed | tr -d '\r')" = "1" ]; do sleep 2; done
+adb devices -l
+
+git clone https://github.com/honeynet/droidbot.git
+cd droidbot
+
+python3 -m pip uninstall -y androguard
+python3 -m pip install "androguard>=3.4.0a1,<4"
+
+python3 -m pip install -e .
+
+echo ">> DONE."
