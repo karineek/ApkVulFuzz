c30bb112d9845c9e56f002ee520e93455c29a926
Karine EM <karineven79@gmail.com>
2026-03-18 21:14:42 +0000

    Update afl_ApkVulFuzz.sh

--- afl_ApkVulFuzz.sh
+++ afl_ApkVulFuzz.sh
@@ -4,11 +4,13 @@
 ## /home/ubuntu/ASEGem5/hello-custom-binary-Ex.py --isa X86 --input @@
 ## python3 start.py -a ../F-Droid.apk -d emulator-5554 -policy bfs_greedy -count 1000 -interval 2 -timeout 1800 -o results/fdroid_run -is_emulator -grant_perm
 
-AFL_SKIP_CPUFREQ=1 AFL_NO_AFFINITY=1 AFL_SHUFFLE_QUEUE=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_DUMB_FORKSRV=1 AFL_FAST_CAL=1 AFL_IGNORE_PROBLEMS=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
+AFL_FAST_CAL=1 AFL_IGNORE_PROBLEMS=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
+AFL_SKIP_CPUFREQ=1 AFL_NO_AFFINITY=1 AFL_DUMB_FORKSRV=1 AFL_SKIP_BIN_CHECK=1 \
 AFL_MAP_SIZE=1200000 \
-AFL_MAX_FILE=20000000 \
-AFL_CUSTOM_MUTATOR_LIBRARY=$HOME/ApkVulFuzz/build/cm-ApkVulFuzz.so \
+AFL_SHUFFLE_QUEUE=1 \
+AFL_CUSTOM_MUTATOR_ONLY=1 AFL_CUSTOM_MUTATOR_LIBRARY=$HOME/ApkVulFuzz/build/cm-ApkVulFuzz.so \
 $HOME/AFLplusplus/afl-fuzz -m 50000 -t 99000 \
+--max_len 20000000 \
 -i $HOME/ApkVulFuzz/Evaluation-SSBSE-2026/corpus/ -o output -- \
 python3 $HOME/droidbot/start.py -a @@ -d emulator-5554 \
 -policy bfs_greedy -count 100 -interval 2 \
