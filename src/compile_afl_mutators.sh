fe14fcc6e37a6f01ce75ef23c71c0124c472df72
Karine EM <karineven79@gmail.com>
2026-03-18 19:38:49 +0000

    Update compile_afl_mutators.sh

--- src/compile_afl_mutators.sh
+++ src/compile_afl_mutators.sh
@@ -1,7 +1,7 @@
 mkdir -p ../build
 cd custom_mutators/
 clang -c -fpic -Wall -O3 -I$HOME/AFLplusplus/include/ $HOME/AFLplusplus/src/afl-performance.c -o afl-performance.o
-clang -c -fpic -Wall -O3 -I$HOME/AFLplusplus/include/ -I . bitflip.c cm_ApkVulFuzz.c -o mutators_afl.o
+clang -c -fpic -Wall -O3 -I$HOME/AFLplusplus/include/ -I . bitflip.c cm_ApkVulFuzz.c -o mutators_afl.o -D AFL_CM
 clang -shared -o cm-ApkVulFuzz.so afl-performance.o mutators_afl.o
 cp *.o ../../build
 cp *.so ../../build
