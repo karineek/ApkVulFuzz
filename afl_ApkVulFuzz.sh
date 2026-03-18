49a484b68ccf3f825feb0b30e32cdbcd086274a4
Karine EM <karineven79@gmail.com>
2026-03-18 21:10:13 +0000

    Update AFL environment variables in script

--- afl_ApkVulFuzz.sh
+++ afl_ApkVulFuzz.sh
@@ -4,7 +4,7 @@
 ## /home/ubuntu/ASEGem5/hello-custom-binary-Ex.py --isa X86 --input @@
 ## python3 start.py -a ../F-Droid.apk -d emulator-5554 -policy bfs_greedy -count 1000 -interval 2 -timeout 1800 -o results/fdroid_run -is_emulator -grant_perm
 
-AFL_NO_AFFINITY=1 AFL_SHUFFLE_QUEUE=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_DUMB_FORKSRV=1 AFL_FAST_CAL=1 AFL_IGNORE_PROBLEMS=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
+AFL_SKIP_CPUFREQ=1 AFL_NO_AFFINITY=1 AFL_SHUFFLE_QUEUE=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_DUMB_FORKSRV=1 AFL_FAST_CAL=1 AFL_IGNORE_PROBLEMS=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
 AFL_MAP_SIZE=1200000 \
 AFL_CUSTOM_MUTATOR_LIBRARY=$HOME/ApkVulFuzz/build/cm-ApkVulFuzz.so \
 $HOME/AFLplusplus/afl-fuzz -m 50000 -t 99000 \
