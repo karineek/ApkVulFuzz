db3c0d3999f66f869ce366a7c549c2279b16a7c5
Karine EM <karineven79@gmail.com>
2026-03-18 21:00:31 +0000

    Create afl_ApkVulFuzz.sh

---
+++ afl_ApkVulFuzz.sh
@@ -0,0 +1,14 @@
+## AFL_NO_AFFINITY=1 AFL_SHUFFLE_QUEUE=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_DUMB_FORKSRV=1 AFL_FAST_CAL=1 AFL_IGNORE_PROBLEMS=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
+## AFL_MAP_SIZE=1200000 AFL_CUSTOM_MUTATOR_LIBRARY="$cm_path/cm-gem5.so" \
+## $afl -m 50000 -t 99000 -i $input -o $output -- /home/ubuntu/gem5-ssbse-challenge-2023/build/X86/gem5.opt \
+## /home/ubuntu/ASEGem5/hello-custom-binary-Ex.py --isa X86 --input @@
+
+AFL_NO_AFFINITY=1 AFL_SHUFFLE_QUEUE=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_DUMB_FORKSRV=1 AFL_FAST_CAL=1 AFL_IGNORE_PROBLEMS=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
+AFL_MAP_SIZE=1200000 \
+AFL_CUSTOM_MUTATOR_LIBRARY=./build/cm-ApkVulFuzz.so \
+afl-fuzz -m none -m 50000 -t 99000 \
+-i input -o output -- \
+python3 start.py -a @@ -d emulator-5554 \
+-policy bfs_greedy -count 1000 -interval 2 \
+-timeout 1800 -o results/fdroid_run \
+-is_emulator -grant_perm
