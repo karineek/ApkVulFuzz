91a1c090415f2ab1063c7308f87130b1117b5a1e
Karine EM <karineven79@gmail.com>
2026-03-18 19:40:21 +0000

    Update compile_mutators.sh

--- src/compile_mutators.sh
+++ src/compile_mutators.sh
@@ -1,4 +1,6 @@
 mkdir -p ../build
 cd custom_mutators/
-clang -I. -o standalone_mutator.o bitflip.c cm_ApkVulFuzz.c
-cp *.o ../../build
+clang -Wall -Wextra -g -fsanitize=address,undefined -I. -o standalone_mutator bitflip.c cm_ApkVulFuzz.c
+cp standalone_mutator ../../build/
+echo ">> Done."
+ls -l ../../build/
