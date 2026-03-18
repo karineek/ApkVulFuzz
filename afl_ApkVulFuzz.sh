2a776cd5f486b37765e09391e59f79b81b39da04
Karine EM <karineven79@gmail.com>
2026-03-18 21:08:45 +0000

    Update afl_ApkVulFuzz.sh

--- afl_ApkVulFuzz.sh
+++ afl_ApkVulFuzz.sh
@@ -6,8 +6,8 @@
 
 AFL_NO_AFFINITY=1 AFL_SHUFFLE_QUEUE=1 AFL_CUSTOM_MUTATOR_ONLY=1 AFL_DUMB_FORKSRV=1 AFL_FAST_CAL=1 AFL_IGNORE_PROBLEMS=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
 AFL_MAP_SIZE=1200000 \
-AFL_CUSTOM_MUTATOR_LIBRARY=./build/cm-ApkVulFuzz.so \
-$HOME/AFLplusplus/afl-fuzz -m none -m 50000 -t 99000 \
+AFL_CUSTOM_MUTATOR_LIBRARY=$HOME/ApkVulFuzz/build/cm-ApkVulFuzz.so \
+$HOME/AFLplusplus/afl-fuzz -m 50000 -t 99000 \
 -i ApkVulFuzz/Evaluation-SSBSE-2026/corpus/ -o output -- \
 python3 $HOME/droidbot/start.py -a @@ -d emulator-5554 \
 -policy bfs_greedy -count 100 -interval 2 \
