9536e779cc6b01ce4ca65740b8f6d6c60d245a2d
Karine EM <karineven79@gmail.com>
2026-03-18 15:10:47 +0000

    Update utils.h

--- src/custom_mutators/utils.h
+++ src/custom_mutators/utils.h
@@ -4,4 +4,7 @@
 // Print error message to screen
 void print_error(const char *msg, const char *data);
 
+// Count the number of lines in a buffer to check no bad mutations are written
+int countLines(const char *str, int _maxsize);
+
 #endif // UTILS_H
