9faa25b602921434e854c149af813b59f66dc6e8
Karine EM <karineven79@gmail.com>
2026-03-18 12:53:05 +0000

    Add bitflip.h header for binary mutation function

---
+++ src/custom_mutators/bitflip.h
@@ -0,0 +1,11 @@
+// bitflip.h header
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifndef BITFLIP_H
+#define BITFLIP_H
+
+// Mutation function
+bool mutateBinary(uint8_t *new_buf, my_mutator_t *data);
+
+#endif // BITFLIP_H
