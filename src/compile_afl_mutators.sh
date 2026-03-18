5613a35c484a2210ca90d6a5892e18d0aba0e703
Karine EM <karineven79@gmail.com>
2026-03-18 19:46:26 +0000

    Update compile_afl_mutators.sh

--- src/compile_afl_mutators.sh
+++ src/compile_afl_mutators.sh
@@ -1,7 +1,27 @@
 mkdir -p ../build
-cd custom_mutators/
-clang -c -fpic -Wall -O3 -I$HOME/AFLplusplus/include/ $HOME/AFLplusplus/src/afl-performance.c -o afl-performance.o
-clang -c -fpic -Wall -O3 -I$HOME/AFLplusplus/include/ -I . bitflip.c cm_ApkVulFuzz.c -o mutators_afl.o -D AFL_CM
-clang -shared -o cm-ApkVulFuzz.so afl-performance.o mutators_afl.o
+cd custom_mutators/ || exit 1
+
+# Compile AFL helper
+clang -c -fPIC -Wall -O3 \
+  -I"$HOME/AFLplusplus/include" \
+  "$HOME/AFLplusplus/src/afl-performance.c" \
+  -o afl-performance.o
+
+# Compile sources (SEPARATELY)
+clang -c -fPIC -Wall -O3 \
+  -I"$HOME/AFLplusplus/include" -I. \
+  -D AFL_CM \
+  bitflip.c -o bitflip.o
+
+clang -c -fPIC -Wall -O3 \
+  -I"$HOME/AFLplusplus/include" -I. \
+  -D AFL_CM \
+  cm_ApkVulFuzz.c -o cm_ApkVulFuzz.o
+
+# Link into shared object
+clang -shared -o cm-ApkVulFuzz.so \
+  afl-performance.o bitflip.o cm_ApkVulFuzz.o
+
+# Copy outputs
 cp *.o ../../build
 cp *.so ../../build
