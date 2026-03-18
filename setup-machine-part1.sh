f8188e3177707cacbaf9f0cc2f78a94787812274
Karine EM <karineven79@gmail.com>
2026-03-18 18:16:48 +0000

    Update setup-machine-part1.sh

--- setup-machine-part1.sh
+++ setup-machine-part1.sh
@@ -25,6 +25,7 @@ sudo ln -s /usr/bin/llvm-config-13 /usr/bin/llvm-config
 git clone https://github.com/AFLplusplus/AFLplusplus.git
 cd AFLplusplus
 RUN git checkout f596a297c4de6a5e1a6fb9fbb3b4e18124a24f58
+cp ../ApkVulFuzz/src/afl-files/afl-fuzz-init.c src/afl-fuzz-init.c
 make all
 sudo make install
 
