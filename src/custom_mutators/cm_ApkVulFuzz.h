e504dbe3c096f05649b809b60770dd4998f33c18
Karine EM <karineven79@gmail.com>
2026-03-18 12:42:18 +0000

    Update cm_ApkVulFuzz.h

--- src/custom_mutators/cm_ApkVulFuzz.h
+++ src/custom_mutators/cm_ApkVulFuzz.h
@@ -13,5 +13,21 @@
 #include <time.h>
 #include <limits.h>
 
+typedef struct my_mutator {
+
+#ifndef AFL_CM
+  afl_state_t *afl;
+#endif
+
+  char* out_buff; // The whole args in buffers
+
+  char* file_name_types; // The name of the file with types to mutate
+
+  char* input_args; // Keeps the input arguments for mutating
+
+  char* input_digit; // Buffer for register mutations
+
+} my_mutator_t;
+
 
 #endif // CUSTOM_MUTATOR_H
