3af0c47f1dcc5feaea5f52d683fe6fce383ed22c
Karine EM <karineven79@gmail.com>
2026-03-18 12:38:05 +0000

    Update and rename ApkVulFuzz_cm.h to cm_ApkVulFuzz.h

---
+++ src/custom_mutators/cm_ApkVulFuzz.h
@@ -0,0 +1,17 @@
+#ifndef CUSTOM_MUTATOR_H
+#define CUSTOM_MUTATOR_H
+
+#ifndef AFL_CM
+  #include "afl-fuzz.h"
+#endif
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <time.h>
+#include <limits.h>
+
+
+#endif // CUSTOM_MUTATOR_H
