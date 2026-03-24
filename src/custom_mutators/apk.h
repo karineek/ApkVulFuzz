10cb8f28546a4ec6a272853494ae81bde2064215
Karine EM <karineven79@gmail.com>
2026-03-24 10:06:36 +0000

    Update apk.h

--- src/custom_mutators/apk.h
+++ src/custom_mutators/apk.h
@@ -1,3 +1,15 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <time.h>
+#include <limits.h>
+#include <stdint.h>
+
+#ifndef _APK_DS_H
+#define _APK_DS_H
+
 typedef struct {
     const char *apk_name;
     uint64_t start_offset;
@@ -5,4 +17,8 @@ typedef struct {
 } ApkEntry;
 extern ApkEntry apk_table[];
 extern const size_t apk_table_size;
+
 const ApkEntry* find_apk(const char *name);
+char *build_output_filename(const char *input_path);
+
+#endif // _APK_DS_H
