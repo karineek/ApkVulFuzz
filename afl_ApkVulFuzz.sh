77861af2ffcc48b6d2e5ccf081135c58c0ed36aa
Karine EM <karineven79@gmail.com>
2026-03-24 15:20:59 +0000

    Update afl_ApkVulFuzz.sh

--- afl_ApkVulFuzz.sh
+++ afl_ApkVulFuzz.sh
@@ -14,3 +14,20 @@ $HOME/AFLplusplus/afl-fuzz -n -m none -t 99000 \
 
 ## python3 /users/kevenmen/droidbot/start.py -aa /users/kevenmen/AFLplusplus/output/.cur_input -d emulator-5554 -policy bfs_greedy -count 100 -interval 2 -timeout 50 -o results/fdroid_run -is_emulator -grant_perm
 ## python3                          start.py -a ../F-Droid.apk                                 -d emulator-5554 -policy bfs_greedy -count 1000 -interval 2 -timeout 1800 -o results/fdroid_run -is_emulator -grant_perm
+
+
+AFL_SKIP_CPUFREQ=1 \
+AFL_SKIP_BIN_CHECK=1 \
+AFL_CUSTOM_MUTATOR_ONLY=1 \
+AFL_SHUFFLE_QUEUE=1 \
+AFL_CUSTOM_MUTATOR_LIBRARY=$HOME/ApkVulFuzz/build/cm-ApkVulFuzz.so \
+AFL_NO_AFFINITY=1 \
+py-afl-fuzz -n -m none -t 99000 \
+          -i $HOME/ApkVulFuzz/Evaluation-SSBSE-2026/input \
+          -o output -- \
+      $HOME/droidbot/start.py -aa @@ -d emulator-5554 \
+      -policy bfs_greedy -count 100 -interval 2 \
+      -timeout 50 -o results/fdroid_run \
+      -is_emulator -grant_perm
+
+### py-afl-fuzz -i /fuzzer_input_corpus -o /OUTPUTS -t 4000 -- /harnesses/differential_harness.py
