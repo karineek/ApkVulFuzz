f2039ff808a5a3d65f512dc8a491bac825a8fb66
Karine EM <karineven79@gmail.com>
2026-03-25 10:31:22 +0000

    Update black_box_fuzz.h

--- src/custom_mutators/black_box_fuzz.h
+++ src/custom_mutators/black_box_fuzz.h
@@ -1,1 +1,10 @@
-todo
+#include <stdlib.h>
+
+#ifndef _BLACK_BOX_FUZZ_H
+#define _BLACK_BOX_FUZZ_H
+
+static int ensure_dir(const char *path);
+static int is_regular_file(const char *path);
+static int run_target(const char *input_path);
+
+#endif // _BLACK_BOX_FUZZ_H
