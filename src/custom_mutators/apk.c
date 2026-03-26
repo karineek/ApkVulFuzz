f27c29acf1bc98f13fd02111adaa924400c8c4e9
Karine EM <karineven79@gmail.com>
2026-03-26 19:14:51 +0000

    Update apk.c

--- src/custom_mutators/apk.c
+++ src/custom_mutators/apk.c
@@ -7,7 +7,7 @@
 
 /////// A table for APK and offsets of AndroidManifest.xml
 ApkEntry apk_table[] = {
-    { "ademar-textlauncher-10", 					  4602,  4602 + 1037 },
+    { "ademar-textlauncher-10", 					4602,  4602 + 1037 },
     { "com-gcstar-scanner-1", 						2463, 2463 + 617 },
     { "com-gcstar-viewer-12", 						3998, 3998 + 984 },
     { "com-java-SmokeReducer-1", 					51861, 51861 + 727 },
